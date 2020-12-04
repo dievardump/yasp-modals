4d56c806c1192777346e3584fddb7f5782f2d47b
Simon Fremaux <dievardump@gmail.com>
2020-12-04 16:49:04 +0100

    update example

--- examples/0_readme-example/src/App.svelte
+++ examples/0_readme-example/src/App.svelte
@@ -1,18 +1,25 @@
 <script>
-  import { Modals, openModal, closeModal } from "../../../src/index.js";
-  import MySimpleModal from "../../components/MySimpleModal.svelte";
+  import { Modals, openModal, closeModal } from '../../../src/index.js';
+  import MySimpleModal from '../../components/MySimpleModal.svelte';
 
   function onClick() {
     openModal(MySimpleModal, {
-      title: "My Modal"
+      title: 'My Modal',
     });
   }
 </script>
 
-<div>
-  <a class="back" href="/">&lt; Back to examples</a>
-</div>
+<div><a class="back" href="/">&lt; Back to examples</a></div>
 
 <main>Main content Blablabl</main>
 <button on:click={onClick}>Open Modal !</button>
-<Modals />
+<Modals
+  options={{ props: { style: `
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+    background-color: rgba(255, 0, 255, .5);
+  ` } }} />
